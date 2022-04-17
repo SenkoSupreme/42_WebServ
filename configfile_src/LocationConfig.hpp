c8f1e9b3a3c208de371858b862217df3d6e6f05e
Mohammed Brija <mbrija@e1r1p1.1337.ma>
2022-04-17 00:34:04 +0000

    these 3 are the only ones ?

---
+++ configfile_src/LocationConfig.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   LocationConfig.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 00:32:50 by mbrija            #+#    #+#             */
+/*   Updated: 2022/04/17 00:33:29 by mbrija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <fstream>
+# include <sstream>
+# include <vector>
+# include <algorithm>
+
+class LocationConfig
+{
+private:
+    /* data */
+public:
+    LocationConfig(/* args */);
+    ~LocationConfig();
+};
+
---
+++ configfile_src/ServerConfig.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrija <mbrija@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 00:29:22 by mbrija            #+#    #+#             */
+/*   Updated: 2022/04/17 00:29:42 by mbrija           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# include <iostream>
+# include <string>
+# include <cstring>
+# include <fstream>
+# include <sstream>
+# include <vector>
+# include <algorithm>
+
+class ServerConfig
+{
+private:
+    /* data */
+public:
+    ServerConfig(/* args */);
+    ~ServerConfig();
+};
+
